966ae004eb6870705f750653692f7be6f8f0f07b
Vantix-1 <vance.frommer@gmail.com>
2025-10-24 18:17:50 -0700

    Update

---
+++ Week_2/Chapter_5/conditional_tests.py
@@ -0,0 +1,33 @@
+# 5-1. Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test.
+#   -Look closely at your results, and make sure you understand why each line evaluates to True or False.
+#   -Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict false.")
+print(car == 'audi')
+
+car = 'toyota'
+print("\nIs car == 'toyota'? I predict True.")
+print(car == 'toyota')
+print("\nIs car == 'audi'? I predict false.")
+print(car == 'audi')
+
+car = 'bmw'
+print("\nIs car == 'bmw'? I predict True.")
+print(car == 'bmw')
+print("\nIs car == 'audi'? I predict false.")
+print(car == 'audi')
+
+car = 'audi'
+print("\nIs car == 'audi'? I predict True.")
+print(car == 'audi')
+print("\nIs car == 'subaru'? I predict false.")
+print(car == 'subaru')
+
+car = 'mercedes'
+print("\nIs car == 'mercedes'? I predict True.")
+print(car == 'mercedes')
+print("\nIs car == 'audi'? I predict false.")
+print(car == 'audi')
